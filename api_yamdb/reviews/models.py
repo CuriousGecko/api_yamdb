fd5a7bca0af33c4a1c7f3e2e71ea644bc292a194
leonid Tsybulsky <axonyx@mail.ru>
2023-10-05 20:18:59 +0300

    Code correction

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from api_yamdb.constants import MAX_LENGHT_NAME, MAX_LENGHT_SLUG, MAX_LENGTH_STR 
+from api_yamdb.constants import (MAX_LENGHT_NAME, MAX_LENGHT_SLUG,
+                                 MAX_LENGTH_STR)
 from reviews.validators import validate_year
 
 User = get_user_model()
