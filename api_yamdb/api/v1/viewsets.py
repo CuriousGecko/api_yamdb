0d82dd86a89a60fa2efe2e73e84a9cda80b6d28c
Lena Konstantinova <smile711@mail.ru>
2023-10-07 15:51:09 +0300

    Created mixins

--- api_yamdb/api/v1/viewsets.py
+++ api_yamdb/api/v1/viewsets.py
@@ -1,6 +1,6 @@
-from django.db.models import prefetch_related_objects
 from rest_framework import filters, mixins, viewsets
-from rest_framework.response import Response
+
+from api.v1.mixins import PatchModelMixin
 
 
 class ListCreateDestroyViewSet(
@@ -19,36 +19,12 @@ class ListCreateDestroyViewSet(
     )
 
 
-class PatchModelMixin:
-    """Миксин частично обновляет ресурс."""
-
-    def perform_patch(self, serializer, **kwargs):
-        serializer.save()
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance,
-            data=request.data,
-            partial=True,
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        queryset = self.filter_queryset(self.get_queryset())
-        if queryset._prefetch_related_lookups:
-            instance._prefetched_objects_cashe = {}
-            prefetch_related_objects(
-                [instance], *queryset._prefetch_related_lookups
-            )
-        return Response(serializer.data)
-
-    def perform_update(self, serializer):
-        serializer.save()
-
-
 class ListCreateRetrievePatchDestroyViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
     PatchModelMixin,
-    ListCreateDestroyViewSet
+    viewsets.GenericViewSet,
 ):
     pass
